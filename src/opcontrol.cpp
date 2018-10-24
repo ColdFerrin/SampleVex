cf62750cedf465c438fe49ec758e4eece7d2bb8c
coldferrin <ferrinkatz@ferrinkatz.com>
2018-10-23 18:50:59 -0700

    Update src/opcontrol.cpp

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -17,7 +17,7 @@
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	pros::Motor right_mtr(4);
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
